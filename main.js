96241c60378a1e5e08954de09628f29f60773fac
Giulia <giulia.guerrazzi@live.it>
2020-11-06 17:21:46 +0100

    welcome vue

---
+++ main.js
@@ -0,0 +1,9 @@
+// introduzione vue
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        title: 'Benvenuti nel mondo di Vue!',
+        img: 'https://www.alessandrocosta.pro/wp-content/uploads/2018/03/guida-vue-js-5.jpg',
+    }
+});
